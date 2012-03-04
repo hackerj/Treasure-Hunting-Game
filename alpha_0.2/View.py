298402832d36c2b5edf36da9e714781fc3ddafb8
glent <glent@8388da33-096f-430c-b107-597122e0fa48>
2012-03-04 01:17:17 +0000

    Added message box for quit
    git-svn-id: https://svn.cs.hmc.edu/svn/cs121sp2012_4/trunk@67 8388da33-096f-430c-b107-597122e0fa48

--- alpha_0.2/View.py
+++ alpha_0.2/View.py
@@ -8,12 +8,12 @@
 from sys import argv, exit               #Get command line arguments
 from PyQt4.QtGui import QApplication     #PyQt Application Window
 from ViewMain import WidMain             #Toplevel Widget
-from Gui import GuiMain as GuiMain  #Gui for Toplevel Widget
+from Gui import GuiMain as GuiMain       #Gui for Toplevel Widget
 
 class View(object):
     def __init__(self, data):
         "Init Window and Gui"
         self.app = QApplication(argv)      #Create Window
         self.widMain = WidMain(data)       #Inherits from QMainWindow
-        self.guiMain  = GuiMain(data)          #Contains buttons, graphics, etc.
+        self.guiMain  = GuiMain(data)      #Contains buttons, graphics, etc.
         self.guiMain.setupUi(self.widMain) #Add ui to main window
--- alpha_0.2/ViewMain.py
+++ alpha_0.2/ViewMain.py
@@ -5,7 +5,7 @@
 # Version: 0.1 using PyQt4.9
 # Wiki_url: https://www.cs.hmc.edu/trac/cs121sp2012_4/
 
-from PyQt4.QtGui import QMainWindow
+from PyQt4.QtGui import QMainWindow, QMessageBox
 from Events import movementEvent
 
 class WidMain(QMainWindow):
@@ -19,3 +19,20 @@ class WidMain(QMainWindow):
     def keyPressEvent(self, event):
         "Get keyboard events no matter what widget has focus"
         movementEvent(self.data, event)
+        
+    def closeEvent(self,event):
+        quit_msg = "Are you sure you want to quit?"
+        reply = QMessageBox()
+        reply.setWindowTitle("Quit Game")
+        reply.setText(quit_msg)
+        reply.setStandardButtons(QMessageBox.Save | QMessageBox.Discard | QMessageBox.Cancel)
+        reply.setDefaultButton(QMessageBox.Save)
+        ret = reply.exec_()
+        
+        if ret == QMessageBox.Discard:
+            event.accept()
+        elif ret == QMessageBox.Cancel:
+            event.ignore()
+        else:
+            self.data.view.guiMain.save_file_dialog()
+            event.accept()
