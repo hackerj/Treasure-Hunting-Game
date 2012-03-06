72155a7d2db8655e03bf41c4d2a74a33c4c56e87
glent <glent@8388da33-096f-430c-b107-597122e0fa48>
2012-03-06 07:15:25 +0000

    Separated sound into its own class
    git-svn-id: https://svn.cs.hmc.edu/svn/cs121sp2012_4/trunk@103 8388da33-096f-430c-b107-597122e0fa48

---
+++ alpha_0.2/Sounds.py
@@ -0,0 +1,40 @@
+# ---------------
+# Map Master Game
+# ---------------
+# Authors: MI Maps Team, CS121-SoftwareDevelopment, Harvey Mudd College
+# Version: 0.2 using PyQt4.9
+# Wiki_url: https://www.cs.hmc.edu/trac/cs121sp2012_4/
+
+from pygame import mixer
+from os.path import normpath
+
+class Sounds(object):
+    """This module handle sound (sets volume and switches sound)"""
+    def __init__(self, initialSlider):
+        #mixer.pre_init(44100, -16, 2, 1024)
+        mixer.init()
+        self.menuSound = normpath("sounds/theme.mp3")
+        self.gameSound = normpath("sounds/gameTheme.wav")
+        self.currSound = self.menuSound
+        mixer.music.load(self.menuSound)
+        mixer.music.play(-1)
+        self.volumeLevel = initialSlider
+        self.setVolume(self.volumeLevel)
+        
+    #Set volume (only works on music right now)
+    def setVolume(self, slider):
+        mixer.music.set_volume((float(slider))/100.0)
+        self.volumeLevel = slider
+        
+    #Change songs (only has songs for menu and game)
+    def switchSongs(self, songnum):
+        mixer.music.fadeout(1000)
+        if songnum == 0:
+            self.currSound = self.menuSound
+        elif songnum == 2:
+            self.currSound = self.gameSound
+        else:
+            self.currSound = self.menuSound
+        mixer.music.load(self.currSound)
+        mixer.music.play(-1)
+        self.setVolume(self.volumeLevel)
