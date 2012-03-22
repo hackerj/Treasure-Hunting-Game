785786fa12da2ffe6562a091d230007d528f2eda
pandrien <pandrien@8388da33-096f-430c-b107-597122e0fa48>
2012-03-22 22:21:27 +0000

    Name change and minor edits
    git-svn-id: https://svn.cs.hmc.edu/svn/cs121sp2012_4/trunk@129 8388da33-096f-430c-b107-597122e0fa48

--- beta_01/Load.py
+++
@@ -1,8 +0,0 @@
-# ---------------
-# Map Master Game
-# ---------------
-# Authors: MI Maps Team, CS121-SoftwareDevelopment, Harvey Mudd College
-# Version: 0.1 using PyQt4.9
-# Wiki_url: https://www.cs.hmc.edu/trac/cs121sp2012_4/
-
-# Nothing here yet
