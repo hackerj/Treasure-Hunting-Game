d37c78a37330d15c60e676b6a2d70b67faa3429d
pandrien <pandrien@8388da33-096f-430c-b107-597122e0fa48>
2012-04-01 06:09:05 +0000

    added empty Loc file and empyt Character File. Declare struture for game
    git-svn-id: https://svn.cs.hmc.edu/svn/cs121sp2012_4/trunk@170 8388da33-096f-430c-b107-597122e0fa48

--- beta_04/Game.py
+++ beta_04/Game.py
@@ -6,3 +6,22 @@ Authors: MI Maps Team, CS121-SoftwareDevelopment, Harvey Mudd College
 Version: 0.4 using PyQt4.9
 Wiki_url: https://www.cs.hmc.edu/trac/cs121sp2012_4/
 """
+
+class Game(Object):
+    "Container to hold one new or loaded game instance"
+
+    def __init__(self, pview, mview, cview):
+        "Init Classes and store references to need gui elements
+
+        #Gui Ouput Links
+        self.pview = pview
+        self.mview = mview
+        self.cview = cview
+        
+    def initNew(self):
+        "Load new game from file"
+        None
+
+    def initSaved(self)
+        "Load existing game from file"
+        None
