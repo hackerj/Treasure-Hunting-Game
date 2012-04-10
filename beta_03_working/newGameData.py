8f179da67026784abc330a31ea69e467eeb34af3
gstafford <gstafford@8388da33-096f-430c-b107-597122e0fa48>
2012-04-10 20:07:09 +0000

    added content
    git-svn-id: https://svn.cs.hmc.edu/svn/cs121sp2012_4/trunk@228 8388da33-096f-430c-b107-597122e0fa48

--- beta_03_working/newGameData.py
+++ beta_03_working/newGameData.py
@@ -18,19 +18,46 @@ commands = {
          {'pos':(-350, 100),'objType':'landmark','pViewImage':'city2.png','mViewImage':'city.png', 'itemName':'city0'},
          {'pos':(-450, 910),'objType':'landmark','pViewImage':'city2.png','mViewImage':'capital.png', 'itemName':'city1'},
          {'pos':(450, -700),'objType':'landmark','pViewImage':'city2.png','mViewImage':'city.png', 'itemName':'city2'},
+
+         {'pos':(-1700, -600),'objType':'landmark','pViewImage':'city2.png','mViewImage':'city.png', 'itemName':'city3'},
+         {'pos':(-1800, 1000),'objType':'landmark','pViewImage':'city2.png','mViewImage':'city.png', 'itemName':'city4'},
+         {'pos':(800, 1200),'objType':'landmark','pViewImage':'city2.png','mViewImage':'capital.png', 'itemName':'city5'},
+
+         {'pos':(-1400, -1000),'objType':'landmark','pViewImage':'city2.png','mViewImage':'capital.png', 'itemName':'city6'},
+         {'pos':(300, -1900),'objType':'landmark','pViewImage':'city2.png','mViewImage':'city.png', 'itemName':'city7'},
+         {'pos':(0, -1300),'objType':'landmark','pViewImage':'city2.png','mViewImage':'capital.png', 'itemName':'city8'},
+         {'pos':(0, -1800),'objType':'landmark','pViewImage':'Forest3.png','mViewImage':'tree.png', 'itemName':'forest0'},
+         {'pos':(-500, 1700),'objType':'landmark','pViewImage':'Forest3.png','mViewImage':'tree.png', 'itemName':'forest1'},
+
          {'pos':(200,158),'objType':'tree'},
          {'pos':(-800,45),'objType':'tree'},
          {'pos':(120,80),'objType':'tree'},
          {'pos':(310,470),'objType':'tree'},
          {'pos':(220, -400),'objType':'tree'},
-         {'pos':(100,500),'objType':'tree'},         
+         {'pos':(100,500),'objType':'tree'},
+
+         {'pos':(-1800, 1600),'objType':'tree'},
+         {'pos':(-1600,1600),'objType':'tree'},
+         {'pos':(500,1100),'objType':'tree'},
+         {'pos':(550,1100),'objType':'tree'},
+         {'pos':(-450, -400),'objType':'tree'},
+         {'pos':(100,1300),'objType':'tree'},    
+                 
         ],
     
     'addClue':
         [
-         {'landmark': 'city1','text': "For this final clue\n Look to the city most southernly"},
-         {'landmark':'city0',  'text': "Check 37.8 deg latitude\n (Distance from Equator)"},
-         {'landmark': 'city2', 'text':  "Search for the second clue \n in the northernmost city"},
+         {'landmark': 'forest0','text': "For this final clue\n search the coldest forest"},
+         {'landmark':'city0',  'text': "Find the next clue\n in a city known for its timber"},
+         {'landmark': 'city5', 'text':  "Search for the next clue \n in the smallest nation's capital"},
+         
+         {'landmark': 'forest1','text': "Find the next clue in the\n most tropical forest"},
+         {'landmark':'city2',  'text': "Check the city that is \n closest to 39 deg latitude"},
+         {'landmark': 'city3', 'text':  "Find the next clue \n in a city close to its capital"},
+
+         {'landmark': 'city4','text': "The next clue lies within the \n same nation\n "},
+         {'landmark':'city1',  'text': "Search the nearest capital\n for the next clue"},
+         {'landmark': 'city5', 'text':  "Search for the second clue \n at 120 deg longitude"},
          
         ],
     
