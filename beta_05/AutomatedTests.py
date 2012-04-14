4345822a13dd88c69465ca9919f17b845e865b84
pandrien <pandrien@8388da33-096f-430c-b107-597122e0fa48>
2012-04-14 21:16:04 +0000

    Head Comment Edits and Added Branch for Working on Scary stuff for background images
    git-svn-id: https://svn.cs.hmc.edu/svn/cs121sp2012_4/trunk@273 8388da33-096f-430c-b107-597122e0fa48

--- beta_05/AutomatedTests.py
+++ beta_05/AutomatedTests.py
@@ -1,3 +1,12 @@
+"""
+ ---------------
+ Map Master Game
+ ---------------
+ Authors: MI Maps Team, CS121-SoftwareDevelopment, Harvey Mudd College
+ Version: 0.5 using PyQt4.9
+ Wiki_url: https://www.cs.hmc.edu/trac/cs121sp2012_4/
+"""
+
 import unittest
 
 class ClassInstantiation(unittest.TestCase):
